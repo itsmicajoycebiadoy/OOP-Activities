1e777a602d2e465e66e9290cd8aeeb2846aab3fe
itsmicajoycebiadoy <biadoymicajoyce@gmail.com>
2024-09-12 15:04:52 +0800

    first commit

---
+++ Activity.java
@@ -0,0 +1,12 @@
+public class Activity {
+    public static void main(String[] args){
+    
+    Dog pet = new Dog();
+
+    System.out.println(pet.bark());
+
+
+
+
+    }
+}
---
+++ Dog.java
@@ -0,0 +1,23 @@
+class Dog{
+    String name;
+    int puppies;
+    boolean isMale;
+    String listofPuppies[] = new String[5];
+    
+    public void setName(String thisName){
+        thisName = name;
+    }
+
+    public String showName(String thisName){
+        return thisName;
+    }
+
+    public String bark(){
+        return "woof woof";
+    }
+
+   
+
+
+
+}
